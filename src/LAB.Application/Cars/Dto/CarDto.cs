0610fff1e3ffdbd21d673def243624b6e3eae976
Kacper Pieniążek <w64885@student.wsiz.edu.pl>
2023-05-29 14:09:54 +0200

    essa commit added cars dtb

---
+++ src/LAB.Application/Cars/Dto/CarDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Abp.Application.Services.Dto;
+using Abp.AutoMapper;
+
+namespace LAB.Cars.Dto {
+    [AutoMapFrom(typeof(Car))]
+    public class CarDto : EntityDto<long>
+    {
+        public bool IsDeleted { get; set; }
+        public DateTime CreationTime { get; set; }
+    }
+}
+
---
+++ src/LAB.EntityFrameworkCore/EntityFrameworkCore/LABDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Abp.Zero.EntityFrameworkCore;
+using LAB.Authorization.Roles;
+using LAB.Authorization.Users;
+using LAB.MultiTenancy;
+using LAB.Cars;
+
+namespace LAB.EntityFrameworkCore
+{
+    public class LABDbContext : AbpZeroDbContext<Tenant, Role, User, LABDbContext>
+    {
+        /* Define a DbSet for each entity of the application */
+        public DbSet<Car> Cars { get; set; }
+        public LABDbContext(DbContextOptions<LABDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
